f4ee4b632a86d45e43e3713f022a8c99599d3811
Talliny Dalla Nora <tallinydn@gmail.com>
2022-05-29 21:34:26 -0300

    first commit

---
+++ 1_INTRO/hello-world/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World React</h1>
+      <p>Mussum Ipsum, cacilds vidis litro abertis. <strong> Nec orci ornare consequat.</strong> Praesent lacinia ultrices consectetur. Sed non ipsum felis.Mé faiz elementum girarzis, nisi eros vermeio.Atirei o pau no gatis, per gatis num morreus.Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus.</p>
+      <p>Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl.Detraxit consequat et quo num tendi nada.Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.Si num tem leite então bota uma pinga aí cumpadi!</p>
+    </div>
+  );
+}
+
+export default App;
+
